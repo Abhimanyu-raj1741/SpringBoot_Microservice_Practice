92c9ef399089675a02887619ce4b8051dff586c4
Abhimanyu <abhimanyu.raj1741@gmail.com>
2024-08-16 00:21:28 +0530

    update

---
+++ 55_Http_Basic_Auth/src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -0,0 +1,15 @@
+package in.ashokit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeRestController {
+ 
+	@GetMapping("/")
+	public String welcomeMsg() {
+		
+		return "Welcome to Spring Boot";
+	}
+	
+}
